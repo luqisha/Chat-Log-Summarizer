1430acc181a63738d91d4ca3b4a1284b52dfc480
luqisha <ashiqul.islam12@gmail.com>
2025-07-05 01:45:19 +0600

    Add function to extract top k keywords

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 from utils.text_utils import load_data, parse_chats
-from utils.chat_analyzer import get_message_stats
+from utils.chat_analyzer import get_message_stats, extract_keywords
 
 chats = load_data()
 parsed_chats = parse_chats(chats)
@@ -8,3 +8,6 @@ print(json.dumps(parsed_chats, indent=2), '\n')
 
 message_stats = get_message_stats(parsed_chats)
 print("Message Statistics: ", json.dumps(message_stats, indent=2), '\n')
+
+top_keywords = extract_keywords(parsed_chats)
+print("Top Keywords: ", top_keywords, '\n')
--- utils/chat_analyzer.py
+++ utils/chat_analyzer.py
@@ -1,3 +1,8 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+
 def get_message_stats(chat_data):
     """
     Get statistics of messages in the chat data. [Req 2.2]
@@ -29,3 +34,40 @@ def get_message_stats(chat_data):
     }
 
     return stats
+
+
+def extract_keywords(chat_data, k=5):
+    """
+    Extract top k keywords from a message by removing stop words and punctuation.
+    [Req 2.3]
+
+    Parameters:
+    data (list): A list of dictionaries, each containing 'speaker' and 'message' keys.
+    k (int): The number of top keywords to extract.
+
+    Returns:
+    list: A list of top k keywords.
+    """
+    if not chat_data:
+        print("No data to extract keywords from.\n")
+        return []
+
+    nltk.download('punkt', quiet=True)
+    nltk.download('stopwords', quiet=True)
+
+    stop_words = set(stopwords.words('english'))
+    word_corpus = []
+
+    for data in chat_data:
+        words = word_tokenize(data['message'])
+        filtered_words = [word.lower() for word in words
+                          if word.isalpha()
+                          and word.lower() not in stop_words]
+        word_corpus.extend(filtered_words)
+
+    word_corpus.sort()
+
+    freq_dist = nltk.FreqDist(word_corpus)
+    top_keywords = [(freq, word) for word, freq in freq_dist.most_common(k)]
+
+    return top_keywords
