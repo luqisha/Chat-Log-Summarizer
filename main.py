77bd1d5e3e4af66c6fe58fee44acee0207fec84a
luqisha <ashiqul.islam12@gmail.com>
2025-07-04 22:10:53 +0600

    Add pretty print for better debug output

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import json
 from utils.text_utils import load_data, parse_chats
 from utils.chat_analyzer import get_message_stats
 
 chats = load_data()
 parsed_chats = parse_chats(chats)
-print(parsed_chats, '\n')
+print(json.dumps(parsed_chats, indent=2), '\n')
 
 message_stats = get_message_stats(parsed_chats)
-print("Message Statistics: ", message_stats)
+print("Message Statistics: ", json.dumps(message_stats, indent=2), '\n')
