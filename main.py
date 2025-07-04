aca295fea81a7684e6a22ca40eddbb7afe347b11
luqisha <ashiqul.islam12@gmail.com>
2025-07-05 02:24:45 +0600

    Remove debug prints

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import json
 from utils.text_utils import load_data, parse_chats
-from utils.chat_analyzer import get_message_stats, extract_keywords
+from utils.chat_analyzer import generate_summary, get_message_stats, extract_keywords
 
 chats = load_data()
 parsed_chats = parse_chats(chats)
-print(json.dumps(parsed_chats, indent=2), '\n')
 
-message_stats = get_message_stats(parsed_chats)
-print("Message Statistics: ", json.dumps(message_stats, indent=2), '\n')
+# print(json.dumps(parsed_chats, indent=2), '\n')
 
-top_keywords = extract_keywords(parsed_chats)
-print("Top Keywords: ", top_keywords, '\n')
+# message_stats = get_message_stats(parsed_chats)
+# print("Message Statistics: ", json.dumps(message_stats, indent=2), '\n')
+
+# top_keywords = extract_keywords(parsed_chats)
+# print("Top Keywords: ", top_keywords, '\n')
+
+summary = generate_summary(parsed_chats)
+print(summary, '\n')
